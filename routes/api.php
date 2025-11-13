8e65c3086311f977aaf637baea1d490e3bf0cea9
Sergey Osokin <sergey.osokin@netfire.eu>
2025-11-13 23:59:01 +0100

    feat: add example links resource routes and user profile endpoint with links.
    - Added Links resource controller with Sanctum authentication middleware
    - Created /profiles/{id} endpoint to retrieve user data with associated short links

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,20 @@
 <?php
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use App\Http\Controllers\Links;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->resource('/links', Links::class );
+
+Route::get("/profiles/{id}", function ($id) {
+    $user = User::find($id);
+    $links = $user->links()->select('short_link')->get();
+    return response()->json([
+        "user"=> $user,
+        "links"=> $links
+    ]);
+});
